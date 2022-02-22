cc464171f850074f89fdf84234db8d3a51c59910
tbmanhhaha <100204741+tbmanhhaha@users.noreply.github.com>
2022-02-22 18:44:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'OG17 iu thương àk!',
+    introDesc: `EM BIẾT EM NHỚ OG LẮM KHÔNG HUHUHUHUHU`,
+    btnIntro: 'Tất nhiên là biết!',
+    title: 'mọi người cũng nhớ em đúng không',
+    desc: 'hohohohohohohoho',
+    btnYes: 'Yé',
+    btnNo: 'Khồng',
+    question:'THẾ LÀ NHỚ EM LẮM ĐÚNG KHÔNG HOHOHOHOHOHOH',
+    btnReply: 'nhỉ nhỉ nhỉ em biết mà',
+    reply: 'đúng thế và chị nminh sẽ cho em 17k',
+    mess: 'Hí hí tuyệt vời 10/10',
+    messDesc: 'Và giờ đến tiết mục quan trọng',
+    btnAccept: 'Hí ',
+    messLink: 'https://youtu.be/hJ6iKKoY0HY'
 }
